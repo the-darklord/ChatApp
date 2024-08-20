f329add94e81aae19b7b1eeb4c4b5fbca2777615
the-darklord <saimihirnath@protonmail.com>
2024-08-20 11:01:38 +0530

    Dockerization Done

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import GenderCheckbox from "./GenderCheckBox";
+import GenderCheckbox from "./GenderCheckbox";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import useSignup from "../hooks/useSignup";
